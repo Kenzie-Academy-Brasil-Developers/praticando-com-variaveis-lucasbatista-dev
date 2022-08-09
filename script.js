5c20b2b9895b6bfc99412750c9d9ab4e2946aa48
lucasbatista-dev <lucas_pivatto@hotmail.com.br>
2022-08-09 15:07:12 -0300

    Entrega de terça pronta

---
+++ script.js
@@ -0,0 +1,9 @@
+let nome = prompt("Digite o seu nome")
+let sobrenome = prompt("Digite o seu sobrenome")
+let anoNascimento = prompt("Digite o ano em que você nasceu")
+let cidade = prompt("Digite a cidade em que você mora")
+let estado = prompt("Digite o Estado")
+let ano = prompt ("Digite em que ano você está falando")
+let idade = (ano) - (anoNascimento)
+
+console.log(`Olá, me chamo ${nome} ${sobrenome}, tenho ${idade} anos e moro em ${cidade} / ${estado} `)
